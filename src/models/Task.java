5d758b74961605cc0a301490dfdd158815c6b3ed
pochi01 <clear_sky_autumn10@yahoo.co.jp>
2018-12-17 00:03:23 +0900

    Add getAllTasks

--- src/models/Task.java
+++ src/models/Task.java
@@ -8,9 +8,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllTasks",
+            query = "SELECT m FROM Task As m ORDER BY m.id DESC"
+            )
+})
+
 @Table(name = "tasks")
 
 public class Task {
@@ -48,6 +58,14 @@ public class Task {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
     }
 
     public Timestamp getCreated_at() {
